4a7fa6af95d631f0eb45f0a64e3fcf379b3826d8
abhisheksaini8967 <shyamtone@gmail.com>
2026-07-16 18:25:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const { google } = require('googleapis');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(__dirname));
 
 // Google Auth Setup - with env fallback
 let auth;
